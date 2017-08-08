258858b8d65a8758ffd9a7fba5016ad9b5a3f6a7
Cameron Epstein <cameron.a.epstein@gmail.com>
2017-08-08 10:54:24 +0100

    declare locations of db and configure plugins

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as RxDB from 'rxdb';
+import { QueryChangeDetector } from 'rxdb';
+import { schema } from './Schema';
+
+// Query change detector is disabled by default
+QueryChangeDetector.enable();
+QueryChangeDetector.enableDebugging();
+
+// Configure adapters to use IndexDB as the storage engine and enable syncing to a remote DB over HTTP
+RxDB.plugin(require('pouchdb-adapter-idb'));
+RxDB.plugin(require('pouchdb-replication'));
+RxDB.plugin(require('pouchdb-adapter-http'));
+
+// Declare the URL of the remote DB and the name of the local one
+const syncURL = 'http://localhost:5984/';
+const dbName = 'chatdb';
 
 class App extends Component {
   render() {
